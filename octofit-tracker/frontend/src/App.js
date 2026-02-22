b4b0c6cd222ef1a879e565dd594fba788bcacb50
jlasanen <jpla@iki.fi>
2026-02-22 02:35:30 +0000

    committing all changes

---
+++ octofit-tracker/frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/octofitapp-small.png" className="App-logo" alt="OctoFit Tracker" />
+        <p>
+          Welcome to OctoFit Tracker
+        </p>
+        <p>
+          Your personal fitness companion
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
